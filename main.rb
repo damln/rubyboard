4c2703caecd48e81f2b6446bf740d6f64ede1e21
Damian Le Nouaille <dam@dln.name>
2016-10-04 10:19:11 +0200

    add comments and debug outputs

--- main.rb
+++ main.rb
@@ -25,74 +25,89 @@ puts "Start MIDI"
 inputs = UniMIDI::Input.all
 inputs.each do |input|
   puts "INPUT: #{input}"
-  puts "INPUT name: #{input.name}"
+  puts "INPUT name:        #{input.name}"
   puts "INPUT pretty_name: #{input.pretty_name}"
-  puts "INPUT type: #{input.type}"
+  puts "INPUT type:        #{input.type}"
 
+  # FROM the code of "micromidi"
+  # case type.to_sym
+  # when :aftertouch, :pressure, :aft then [MIDIMessage::ChannelAftertouch, MIDIMessage::PolyphonicAftertouch]
+  # when :channel_aftertouch, :channel_pressure, :ca, :cp then MIDIMessage::ChannelAftertouch
+  # when :control_change, :cc, :c then MIDIMessage::ControlChange
+  # when :note, :n then [MIDIMessage::NoteOn, MIDIMessage::NoteOff]
+  # when :note_on, :nn then MIDIMessage::NoteOn
+  # when :note_off, :no, :off then MIDIMessage::NoteOff
+  # when :pitch_bend, :pb then MIDIMessage::PitchBend
+  # when :polyphonic_aftertouch, :poly_aftertouch, :poly_pressure, :polyphonic_pressure, :pa, :pp then MIDIMessage::PolyphonicAftertouch
+  # when :program_change, :pc, :p then MIDIMessage::ProgramChange
+  # end
+
+  # input = UniMIDI::Input.all[0]
   MIDI.using(input) do
-    # FROM the code of "micromidi"
-    # case type.to_sym
-    # when :aftertouch, :pressure, :aft then [MIDIMessage::ChannelAftertouch, MIDIMessage::PolyphonicAftertouch]
-    # when :channel_aftertouch, :channel_pressure, :ca, :cp then MIDIMessage::ChannelAftertouch
-    # when :control_change, :cc, :c then MIDIMessage::ControlChange
-    # when :note, :n then [MIDIMessage::NoteOn, MIDIMessage::NoteOff]
-    # when :note_on, :nn then MIDIMessage::NoteOn
-    # when :note_off, :no, :off then MIDIMessage::NoteOff
-    # when :pitch_bend, :pb then MIDIMessage::PitchBend
-    # when :polyphonic_aftertouch, :poly_aftertouch, :poly_pressure, :polyphonic_pressure, :pa, :pp then MIDIMessage::PolyphonicAftertouch
-    # when :program_change, :pc, :p then MIDIMessage::ProgramChange
-    # end
-
     receive :aftertouch do |message|
-      puts("---------> aftertouch")
-      puts(message)
+      puts("++++++++++ aftertouch")
+      # puts(message)
     end
-
     receive :channel_aftertouch do |message|
-      puts("---------> channel_aftertouch")
-      puts(message)
+      puts("++++++++++ channel_aftertouch")
+      # puts(message)
     end
-
     receive :control_change do |message|
-      puts("---------> control_change, value: #{message.data}")
-      puts("---------> control_change, value: #{message.channel}")
-      puts("---------> control_change, value: #{message.name}")
+      puts("++++++++++ control_change:")
+      puts("---------> control_change, message.data:    #{message.data}")
+      puts("---------> control_change, message.channel: #{message.channel}")
+      puts("---------> control_change, message.name:    #{message.name}")
       dynamic_load
       Keyboard.new.run(message)
     end
-
     receive :note do |message|
-      puts("---------> note, value: #{message.note_name}")
-      dynamic_load
-      Keyboard.new.run(message)
+      puts("++++++++++ note:")
+      puts("---------> note, message.channel:   #{message.channel}")
+      puts("---------> note, message.note:      #{message.note}")
+      puts("---------> note, message.note_name: #{message.note_name}")
+      puts("---------> note, message.velocity:  #{message.velocity}")
+      puts("---------> note, message.octave:    #{message.octave}")
+      puts("---------> note, message.status:    #{message.status}")
+
+      # Velocity 0 is when note is "off":
+      if message.velocity > 0
+        dynamic_load
+        Keyboard.new.run(message)
+      end
     end
 
-    receive :note_on do |message|
-      puts("---------> note_on, value: #{message.note_name}")
-      puts(message)
+    receive :off do |message|
+      puts("++++++++++ off:")
+      puts("---------> off, message.channel:   #{message.channel}")
+      puts("---------> off, message.note:      #{message.note}")
+      puts("---------> off, message.note_name: #{message.note_name}")
+      puts("---------> off, message.velocity:  #{message.velocity}")
+      puts("---------> off, message.octave:    #{message.octave}")
     end
-
-    receive :note_off do |message|
-      puts("---------> note_off, value: #{message.note_name}")
-      puts(message)
-    end
-
+    # receive :note_on do |message|
+    #   puts("---------> note_on, value: #{message.note_name}")
+    #   puts(message)
+    # end
+    # receive :note_off do |message|
+    #   puts("---------> note_off, value: #{message.note_name}")
+    #   puts(message)
+    # end
     receive :pitch_bend do |message|
-      puts("---------> pitch_bend, value: #{message.data}")
-      puts("---------> pitch_bend, value: #{message.channel}")
-      puts(message)
+      puts("++++++++++ pitch_bend:")
+      puts("---------> pitch_bend, message.data:    #{message.data}")
+      puts("---------> pitch_bend, message.channel: #{message.channel}")
+      # puts(message)
     end
-
     receive :polyphonic_aftertouch do |message|
-      puts("---------> polyphonic_aftertouch")
-      puts(message)
+      puts("++++++++++ polyphonic_aftertouch")
+      # puts(message)
     end
-
     receive :program_change do |message|
-      puts("---------> program_change")
-      puts(message)
+      puts("++++++++++ program_change")
+      # puts(message)
     end
 
     join
   end
 end
+
